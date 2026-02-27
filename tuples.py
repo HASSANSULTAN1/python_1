795ff910da60a215ef19a550f9590cc2faf9245d
hassan <hassansultann447@gmail.com>
2026-02-27 23:25:18 +0500

    practice tuple

--- tuples.py
+++ tuples.py
@@ -1,2 +1,12 @@
-tuple_1 = ("apple", "banana", "cherry", "apple", "cherry")
+
+# creating a tuple
+tuple_1 = ("apple", "banana", "cherry", "apple", "cherry",100,6678,False)
 print(tuple_1)
+
+#printing through index
+
+print(tuple_1[4],tuple_1[0])
+
+
+print (len(tuple_1))
+
