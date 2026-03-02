1eb8ee67fd50507414bf883b85e050d19869bbf5
hassan <hassansultann447@gmail.com>
2026-03-02 19:02:26 +0500

    make a if-else.py file

---
+++ if-else.py
@@ -0,0 +1,90 @@
+
+
+# if condition in python
+
+a=18
+b=33
+
+if b>a:
+    print("Hello World")
+
+    if a<b:
+        print("WW3")
+
+
+#elif in python
+
+a = 200
+b = 200
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")   
+
+
+
+score = 82
+
+if score >= 90:
+  print("Grade: A")
+elif score >= 80:
+  print("Grade: B")
+elif score >= 70:
+  print("Grade: C")
+elif score >= 60:
+  print("Grade: D")
+
+
+
+#else statment in python
+  
+score = 44
+
+if score >= 90:
+  print("Grade: A")
+elif score >= 80:
+  print("Grade: B")
+elif score >= 70:
+  print("Grade: C")
+elif score >= 60:
+  print("Grade: D")
+else:
+   print("Grade: F")
+
+
+
+#Logical Operaters in Python
+   
+
+#AND Statment
+   
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
+
+#OR Statment
+
+a = 200
+b = 33
+c = 100
+if a > b or c > a:
+  print("Only one statment is true")   
+
+
+#NOT Operator
+
+a = 66
+b = 200
+if not a > b:
+  print("a is not greater than b")
+
+#using logical operators in if else statments
+  
+score = 85
+
+if score >= 50 and score <= 100:
+  print(" Good score")
+else:
+  print("Bad score")
