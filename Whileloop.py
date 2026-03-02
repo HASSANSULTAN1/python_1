09871b77d38f00c46b33f2fbf928c7056ba441ff
hassan <hassansultann447@gmail.com>
2026-03-02 19:23:29 +0500

    add a whileloop.py file

---
+++ Whileloop.py
@@ -0,0 +1,29 @@
+
+
+#While Loop
+
+a=10
+
+while a<20:
+    print(a)
+    a += 1
+
+
+#using the break statment
+
+b = 5
+while b < 12:
+  print(b)
+  if b == 10:
+    break
+  b += 1    
+
+
+ #if-else in while loop
+
+c = 5
+while c < 10:
+  print(c)
+  c += 1
+else:
+  print("c is no longer less than 10") 
