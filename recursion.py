913a981c41dbffe45c99a351b286863cab43a24d
hassan <hassansultann447@gmail.com>
2026-03-07 06:56:58 +0500

    learning base case and recursive case

--- recursion.py
+++ recursion.py
@@ -1,3 +1,5 @@
+#01
+
 def func_1(n):
     if n <= 10:
         print ("Hello World")
@@ -5,4 +7,15 @@ def func_1(n):
         print (n)
         func_1(n-1)    
 
-func_1(20)                
+func_1(20)  
+
+#02
+
+def fac_01(n):
+    if n <= 1 :
+        return n
+    else:
+          return n * fac_01(n-1)
+
+
+print (fac_01(20))
