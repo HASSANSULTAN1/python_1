3ea63a7d2ada7ff60904c3d6f681969b01c1cb32
hassan <hassansultann447@gmail.com>
2026-03-07 08:17:14 +0500

    learning fabi series

--- recursion.py
+++ recursion.py
@@ -19,3 +19,17 @@ def fac_01(n):
 
 
 print (fac_01(20))
+
+
+
+#03
+
+def find_max(num):
+  if len(num) == 1:
+    return num[0]
+  else:
+    max_num = find_max(num[1:])
+    return num [0] if num[0]>max_num else max_num
+  
+list_1 = [6,99,0,22,55]
+print (find_max(list_1))
