b217e0f94f7458d9dffd889a5dc9039558f0766f
hassan <hassansultann447@gmail.com>
2026-03-09 16:19:48 +0500

    use try,excepy,finally

--- try-else.py
+++ try-else.py
@@ -1,9 +1,13 @@
+#try , except
+
 
 try:
     print(x)
 except:
     print("x is not defined")   
 
+
+#try , except , else
 
 try:
     ("print hello")
@@ -12,4 +16,13 @@ except:
     ("something went wrong")
 
 else:
-    print("everything is all right")        
+    print("everything is all right")  
+
+#try , except , else , finally 
+
+try:
+  print(x)
+except:
+  print("Something went wrong")
+finally:
+  print("it skips the try-except part")             
