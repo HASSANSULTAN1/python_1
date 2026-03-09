b42ef614fd92d31d12626b6087b7af14e15e19a1
hassan <hassansultann447@gmail.com>
2026-03-09 16:17:39 +0500

    use try,except,else

--- try-else.py
+++ try-else.py
@@ -2,4 +2,14 @@
 try:
     print(x)
 except:
-    print("x is not defined")    
+    print("x is not defined")   
+
+
+try:
+    ("print hello")
+
+except:
+    ("something went wrong")
+
+else:
+    print("everything is all right")        
