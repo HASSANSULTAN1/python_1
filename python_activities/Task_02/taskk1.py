bf7505628f6c80b72788c60222d3cb40f0e6a8cf
hassan <hassansultann447@gmail.com>
2026-03-28 09:15:39 +0500

    solve pq1

--- python_activities/Task_02/taskk1.py
+++ python_activities/Task_02/taskk1.py
@@ -1,1 +1,10 @@
-print("hi")
+
+
+
+def is_substring_of_either():
+    s1 = input("Enter first string: ")
+    s2 = input("Enter second string: ")
+    return s1 in s2 or s2 in s1
+
+result = is_substring_of_either()
+print(result)
