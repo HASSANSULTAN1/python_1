f6d98e9cc8fdf0b6316f3367ff132272bc399792
hassan <hassansultann447@gmail.com>
2026-03-28 09:27:54 +0500

    solve q2

--- python_activities/Task_02/taskk2.py
+++ python_activities/Task_02/taskk2.py
@@ -1,4 +1,16 @@
-def my_function():
-  print("Hello from a function")
 
-my_function()
+
+def prime_factors(n):
+    factors = []
+    divisor = 2
+    
+    while divisor <= n:
+        while n % divisor == 0:
+            factors.append(divisor)
+            n //= divisor
+        divisor += 1
+    
+    return factors
+
+n = int(input("enter a number: "))
+print("Prime factors of " + str(n) + ": " + str(prime_factors(n)))
